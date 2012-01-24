36fec8474e2cc40ed42ece8033956983d2040e22
Davide Sottara <dsotty@gmail.com>
2012-01-25 00:41:34 +0100

    Fixing error handling bug

--- drools-mas-core/src/main/java/org/drools/mas/mock/MockFact.java
+++ drools-mas-core/src/main/java/org/drools/mas/mock/MockFact.java
@@ -38,26 +38,6 @@ public class MockFact implements Serializable {
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        MockFact person = (MockFact) o;
-
-        if (age != person.age) return false;
-        if (name != null ? !name.equals(person.name) : person.name != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = name != null ? name.hashCode() : 0;
-        result = 31 * result + age;
-        return result;
-    }
-
     public String getName() {
         return name;
     }
@@ -72,5 +52,25 @@ public class MockFact implements Serializable {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MockFact)) return false;
+
+        MockFact mockFact = (MockFact) o;
+
+        if (age != mockFact.age) return false;
+        if (name != null ? !name.equals(mockFact.name) : mockFact.name != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
     }
 }
